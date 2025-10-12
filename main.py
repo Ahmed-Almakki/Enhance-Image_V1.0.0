afd35c2e4ab834409284e01f835f7705ecc8aea4
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-10-13 00:08:27 +0200

    first version of preprocessing

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 """Enhance Image Using Tensorflow"""
-import cv2
+from PIL import Image
+from matplotlib import pyplot as plt
 import numpy as np
 import os
+import tensorflow as tf
+
+train_path = 'Train_x'
+output_path = 'Train_Y'
+valid_path = 'Valid_x'
+valid_output_path = 'Valid_Y'
+
+train_img = tf.data.Dataset.list_files(train_path + '/*.*', shuffle=False)
+output_img = tf.data.Dataset.list_files(output_path + '/*.*', shuffle=False)
+valid_img = tf.data.Dataset.list_files(valid_path + '/*.*', shuffle=False)
+valid_output = tf.data.Dataset.list_files(valid_output_path + '/*.*', shuffle=False)
+
+Training_images = tf.data.Dataset.zip((train_img, output_img))
+Validating_images = tf.data.Dataset.zip((valid_img, valid_output))
 
 
-def resizeImage(output: bool, img: np.ndarray):
-    """Resizing the image to either 215x215 or 860x860
-        :param
-        output: if True resize output to 860 if no to 215"""
-    if output:
-        return cv2.resize(img, (860, 860), interpolation=cv2.INTER_AREA)
-    return cv2.resize(img, (215, 215), interpolation=cv2.INTER_AREA)
 
+def load_image(image_path, output_image_path):
+    """Load the image to create the pipline
+    :param:
+        image_path: the path of the input image
+        output_path: path of the output image"""
+    print(f"first {image_path}\t{output_image_path}")
+    lr = tf.image.decode_image(tf.io.read_file(image_path), channels=3)
+    hr = tf.image.decode_image(tf.io.read_file(output_image_path), channels=3)
+
+    lr.set_shape([None, None, 3])
+    hr.set_shape([None, None, 3])
+
+    print(f"second {lr}\n{hr}")
+    lr = tf.image.convert_image_dtype(lr, tf.float32)
+    hr = tf.image.convert_image_dtype(hr, tf.float32)
+
+    print(f"\n-------------------------------------\nthird{lr}\n{hr}")
+    lr = tf.image.resize(lr, [215, 215])
+    hr = tf.image.resize(hr, [215, 215])
+    return lr, hr
+
+
+Train_dataset = Training_images.map(load_image)
+Valid_dataset = Validating_images.map(load_image)
+# print(f"train_img: {train_img}\nTraining_image: {Training_images}\nTrainDataset: {Train_dataset}")
+# for x, y in Train_dataset:
+#     print(f"shap: {x.shape}\n x: {x}\n y: {y}")
+#     plt.imshow(y, interpolation='nearest')
+#     plt.show()
+#     break
