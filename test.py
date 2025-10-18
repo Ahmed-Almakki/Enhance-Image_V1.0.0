f165b9717c36e1e1eac186d338fed830b9117d4d
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-10-18 23:48:44 +0200

    removing some files

--- test.py
+++ test.py
@@ -1,45 +1,29 @@
-from matplotlib import pyplot as plt
-import cv2
-from PIL import Image
 import tensorflow as tf
-import numpy as np
+import cv2
 
-model = tf.keras.models.load_model('enhance_model_v1.0.2.h5',  compile=False)
-image_file = 'Valid_x/0813x4m.png'
+# 2. Load trained weights
+model = tf.keras.models.load_model('enhance_model_v1.0.3.h5',
+                                   custom_objects={
+                                       'mse': tf.keras.losses.MeanSquaredError(),
+                                       'mae': tf.keras.metrics.MeanAbsoluteError()
+                                   }
+                                   )
 
+# 3. Load and preprocess the image
 
-def procrss_image(path, model):
-    img = tf.image.decode_image(tf.io.read_file(path), channels=1)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    img = tf.expand_dims(img, axis=0)
-    img = tf.image.resize(img, [215, 215])
-    enhanced = model.predict(img)
-    enhanced = enhanced[0]
-    return enhanced
+image_file = 'Valid_x/0801x4m.png'
+kk = cv2.imread(image_file)
+cv2.imshow('asd', kk)
+img = tf.image.decode_image(tf.io.read_file(image_file), channels=3)
+img = tf.image.convert_image_dtype(img, tf.float32)
+img = tf.expand_dims(img, axis=0)
 
-
-new_image = procrss_image(image_file, model)
-print(new_image.shape)
-# new_image_uint8 = np.clip(new_image * 255, 0, 255).astype(np.uint8)
-
-# Convert RGB to BGR for OpenCV
-new_image_bgr = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
-
-cv2.imshow("Enhanced Image", new_image_bgr)
+# 4. Predict
+enhanced = model.predict(img)[0]
+enhanced = cv2.cvtColor(enhanced, cv2.COLOR_RGB2BGR)
+enhanced = tf.cast(tf.clip_by_value(enhanced * 255.0, 0, 255), tf.uint8)
+print(f'img size{enhanced.shape}\t{type(enhanced)}')
+xx = cv2.imwrite('add.png', enhanced.numpy())
+cv2.imshow("Enhanced Image", enhanced.numpy())
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-# old_image = cv2.imread(image_file)
-# img1 = cv2.cvtColor(old_image, cv2.COLOR_BGR2RGB)
-# img1 = cv2.resize(img1, (215, 215)) / 255.0
-
-# plt.figure(figsize=(10, 5))
-# plt.subplot(1, 2, 1)
-# plt.imshow(img1)
-# plt.title('Blur Image')
-# plt.axis('off')
-#
-#
-# plt.subplot(1, 2, 2)
-# plt.imshow(new_image)
-# plt.title('Enhanced Image')
-# plt.axis('off')
